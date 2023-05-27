bfe683e94dc6151cee4d42424292dd14f32d0667
zidan-p <zidanputrarahman153@gmail.com>
2023-05-27 13:00:47 +0700

    Bind acation creator

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ const redux = require("redux");
 
 
 const createStore = redux.createStore; 
+const bindActionCreators = redux.bindActionCreators;
+
+// # bind action creators
+// sometimes when we working with old version of redux
+// we may meet dificulty when exporting action creator.
+// when we export 1 or 2, that's not a problem.
+// but, if there are so many action it will be a dificult.
+
+// so, woth bind action creator we can bind it action creator with 
+// it's store. it will be easy if want to use some of the action
+
 
 // # all action name
 const CAKE_ORDERED = "CAKE_ORDERED"; 
@@ -49,15 +60,26 @@ console.log("initial state", store.getState()); // log when state first initiali
 const unsubscribe =  store.subscribe(()=>console.log("update state ",store.getState())) 
 // NOTE : subscribe return function than unsubscribe action
 
-// # ordering
-store.dispatch(orderCake()) 
-store.dispatch(orderCake()) 
-store.dispatch(orderCake()) 
-store.dispatch(orderCake()) 
-store.dispatch(orderCake());
+// // # ordering
+// store.dispatch(orderCake()) 
+// store.dispatch(orderCake()) 
+// store.dispatch(orderCake()) 
+// store.dispatch(orderCake()) 
+// store.dispatch(orderCake());
 
-// # restocking
-store.dispatch(restockCake(5));
+// // # restocking
+// store.dispatch(restockCake(5));
+
+
+// we can have same output with bind action
+const actions = bindActionCreators({orderCake, restockCake},store.dispatch);
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+
+actions.restockCake(5);
 
 unsubscribe();
 // when use dispatch after unsubscibe, it won't show any updated log value.
