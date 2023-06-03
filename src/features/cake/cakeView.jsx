a1ebc92db357654bb2e0bae0d72b039ad1e08a84
zidan-p <zidanputrarahman153@gmail.com>
2023-06-03 10:28:43 +0700

    useDispatch

--- src/features/cake/cakeView.jsx
+++ src/features/cake/cakeView.jsx
@@ -1,18 +1,29 @@
 import React from "react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { ordered, restocked } from "./cakeSlice"
 
 export const CakeView = () => {
 
     const numOfCakes = useSelector(state => state.cake.numOfCakes)
-
+    const dispatch = useDispatch();
     return (
         <div className="p-10 border rounded shadow bg-white">
             <h2 className="text-2xl font-bold text-gray-800 mb-2">Number of cake - 
                 <span className="bg-gray-800 text-white px-2 rounded">{numOfCakes}</span>
             </h2>
             <div className="flex gap-2">
-                <button className="px-3 bg-gray-200 rounded-sm hover:bg-gray-300 active:bg-gray-400">Order Cake</button>
-                <button className="px-3 bg-gray-200 rounded-sm hover:bg-gray-300 active:bg-gray-400">Restock Cake</button>
+                <button 
+                    onClick={() => dispatch(ordered())}
+                    className="px-3 bg-gray-200 rounded-sm hover:bg-gray-300 active:bg-gray-400"
+                >
+                    Order Cake
+                </button>
+                <button 
+                    onClick={() => dispatch(restocked(5))}
+                    className="px-3 bg-gray-200 rounded-sm hover:bg-gray-300 active:bg-gray-400"
+                >
+                    Restock Cake
+                </button>
             </div>
         </div>
     )
--- src/features/icecream/icecreamView.jsx
+++ src/features/icecream/icecreamView.jsx
@@ -1,9 +1,11 @@
 import React from "react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { ordered,restocked } from "./icecreamSlice"
 
 export const IcecreamView = () => {
 
     const numOfIcecream = useSelector(state => state.iceCream.numOfIcecream)
+    const dispatch = useDispatch();
 
     return (
         <div className="p-10 border rounded shadow bg-white">
@@ -11,8 +13,18 @@ export const IcecreamView = () => {
                 <span className="bg-gray-800 text-white px-2 rounded">{numOfIcecream}</span>
             </h2>
             <div className="flex gap-2">
-                <button className="px-3 bg-gray-200 rounded-sm hover:bg-gray-300 active:bg-gray-400">Order Ice Cream</button>
-                <button className="px-3 bg-gray-200 rounded-sm hover:bg-gray-300 active:bg-gray-400">Restock Ice Cream</button>
+            <button 
+                    onClick={() => dispatch(ordered())}
+                    className="px-3 bg-gray-200 rounded-sm hover:bg-gray-300 active:bg-gray-400"
+                >
+                    Order Ice Cream
+                </button>
+                <button 
+                    onClick={() => dispatch(restocked(5))}
+                    className="px-3 bg-gray-200 rounded-sm hover:bg-gray-300 active:bg-gray-400"
+                >
+                    Restock Ice Cream
+                </button>
             </div>
         </div>
     )
